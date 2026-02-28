db1aaa1e946425d94910f4805c352892954d401e
ramya23000505 <149370791+ramya23000505@users.noreply.github.com>
2026-02-28 14:07:30 +0530

    Time: 5 ms (77.57%), Space: 45.9 MB (73.51%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+           return false;
+        }
+        int xcopy=x;
+        int rev=0;
+        while(x>0){
+            rev=(rev*10) + (x%10);
+            x/=10;
+        }
+        return xcopy==rev;
+    }
+}
